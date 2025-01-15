4852dc1cf095dfb69a105e9d6fb1d072c0e8aca7
itshaniish <itshaniish@gmail.com>
2025-01-16 00:36:26 +0530

    updated poster and fixed image bug

--- models/Posts.js
+++ models/Posts.js
@@ -5,6 +5,10 @@ const postSchema=new mongoose.Schema({
     type: String,
     required: true,
   },
+  poster: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
