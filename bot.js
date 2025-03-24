f7a52c8b176906a5964b255e395e293697733e2a
its-haniish <itshaniish@gmail.com>
2025-03-24 14:27:20 +0530

    added moveis in public folder

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
-const axios = require('axios');
+const axios=require('axios');
 const puppeteer=require("puppeteer");
 const mongoose=require("mongoose");
 const { Posts, Pages }=require("./models/BotPosts.js");
-const path = require('path');
-const fs = require("fs");
-const SITE= process.env.SITE_URL;
+const path=require('path');
+const fs=require("fs");
+const SITE="https://allmovieshub.ch/";
 
 const main=async () => {
     let browser;
     try {
         browser=await puppeteer.launch({
             headless: true,
-            executablePath: '/snap/bin/chromium', // Use the correct Chromium path here
+            // executablePath: '/snap/bin/chromium', // Use the correct Chromium path here
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
@@ -28,10 +28,10 @@ const main=async () => {
         // Scrape the page for posts
         const scrapePage=async () => {
             const postList=await page.evaluate(() => {
-                let posts=Array.from(document.querySelectorAll('.post-item'));
+                let posts=Array.from(document.querySelectorAll('.post'));
                 return posts.map(post => {
                     let postUrl=post.querySelector('a').href;
-                    let title=post.querySelector('h3').innerText;
+                    let title=post.querySelector('a').title;
                     let poster=post.querySelector('img').src;
                     return { postUrl, title, poster };
                 });
@@ -52,34 +52,31 @@ const main=async () => {
                         }=post;
                         const unwantedWords=["|", "480p", "720p", "1080p", "hindi", "english", "download", "complete", "&"];
                         const spans=Array.from(document.querySelectorAll('span'));
-                        const images=Array.from(document.querySelectorAll('img[width="850"]'));
+                        const strongs=Array.from(document.querySelectorAll('strong'));
+                        const images=Array.from(document.querySelectorAll('.size-full'));
 
                         const featuredImage=images[0]?.src||poster;
-                        const imdbRating=spans.find((elem) => elem.innerText.trim()==="IMDb Rating")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const releaseYear=spans.find((elem) => elem.innerText.trim()==="Release Year")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const genres=spans.find((elem) => elem.innerText.trim()==="Genre")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const format=spans.find((elem) => elem.innerText.trim()==="Format")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const language=spans.find((elem) => elem.innerText.trim()==="Language")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const starCast=spans.find((elem) => elem.innerText.trim()==="Star Cast")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const director=spans.find((elem) => elem.innerText.trim()==="Director")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const fileSize=spans.find((elem) => elem.innerText.trim()==="File Size")?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
-                        const quality=spans.find((elem) => elem.innerText.trim()==="Quality")?.parentElement?.parentElement?.innerText.split(":")[1]?.trim()||"N/A";
+                        const imdbRating=strongs.find((elem) => elem.innerText.trim()==="IMDb Rating")?.parentElement.parentElement.innerText.replace("IMDb Rating", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const releaseYear=strongs.find((elem) => elem.innerText.trim()==="Release Year")?.parentElement.parentElement.innerText.replace("Release Year", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const genres=strongs.find((elem) => elem.innerText.trim()==="Genre")?.parentElement.parentElement.innerText.replace("Genre", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const format=strongs.find((elem) => elem.innerText.trim()==="Format")?.parentElement.parentElement.innerText.replace("Format", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const language=strongs.find((elem) => elem.innerText.trim()==="Language")?.parentElement.parentElement.innerText.replace("Language", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const starCast=strongs.find((elem) => elem.innerText.trim()==="Star Cast")?.parentElement.parentElement.innerText.replace("Star Cast", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const director=strongs.find((elem) => elem.innerText.trim()==="Director")?.parentElement.parentElement.innerText.replace("Director", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const fileSize=strongs.find((elem) => elem.innerText.trim()==="File Size")?.parentElement.innerText.replace("File Size", "").trim().replace(/^:\s*/, "")||"N/A";
+                        const quality=strongs.find((elem) => elem.innerText.trim()==="Quality")?.parentElement.parentElement.innerText.replace("Quality", "").trim().replace(/^:\s*/, "")||"N/A";
                         let image=images[1]?.src;
                         if (!image) {
-                            image=spans.find((elem) => elem.innerText.trim().includes('Screenshots: “See Before Downloading”'))?.parentElement?.nextElementSibling?.nextElementSibling?.getElementsByTagName("img")[0]?.src
-                            if (!image) {
-                                image=spans.find((elem) => elem.innerText.trim().includes('Screenshots: “See Before Downloading”'))?.parentElement?.nextElementSibling?.querySelector("img")?.src||"Image not found.";
-                            }
+                            image=images[0]?.src;
                         }
                         const slug=location.pathname.split('/').filter(Boolean).pop();
                         const words=title.toLowerCase().split(" ");
                         const keywords=words.filter(word => !unwantedWords.includes(word));
                         const metaDesc=`${title} in HD quality for free.`;
-                        const synopsis=spans.find((e) => e.innerText.includes("SYNOPSIS"))?.parentElement?.parentElement?.nextElementSibling?.getElementsByTagName("p")[0]?.innerText||"No synopsis available";
-
+                        const synopsis=strongs.find((e) => e.innerText.includes("Movie-SYNOPSIS/Story"))?.parentElement.nextElementSibling.innerText||"No synopsis available";
                         const ems=Array.from(document.querySelectorAll('em'));
                         const downloadLinks=ems.slice(1).map(em => {
-                            return { type: em.innerText, link: em.parentElement?.href };
+                            return { type: em?.parentElement.innerText, link: em?.parentElement.href };
                         });
 
                         return {
@@ -105,36 +102,36 @@ const main=async () => {
                     }, post);
 
                     // Function to download image
-                    const downloadImage = async (url, savePath) => {
-                        const response = await axios({
-                        url,
-                        method: 'GET',
-                        responseType: 'stream',
+                    const downloadImage=async (url, savePath) => {
+                        const response=await axios({
+                            url,
+                            method: 'GET',
+                            responseType: 'stream',
                         });
-                    
+
                         return new Promise((resolve, reject) => {
-                        const stream = response.data.pipe(fs.createWriteStream(savePath));
-                        stream.on('finish', resolve);
-                        stream.on('error', reject);
+                            const stream=response.data.pipe(fs.createWriteStream(savePath));
+                            stream.on('finish', resolve);
+                            stream.on('error', reject);
                         });
                     };
-                    
+
                     // Ensure the directory exists
-                    const dirPoster = path.join(__dirname, 'public' ,'images', 'poster');
+                    const dirPoster=path.join(__dirname, 'public', 'movies', 'poster');
                     if (!fs.existsSync(dirPoster)) {
                         fs.mkdirSync(dirPoster, { recursive: true });
                     }
-                    
+
                     // Ensure the directory exists
-                    const dirImage = path.join(__dirname, 'public','images', 'image');
+                    const dirImage=path.join(__dirname, 'public', 'movies', 'image');
                     if (!fs.existsSync(dirImage)) {
                         fs.mkdirSync(dirImage, { recursive: true });
                     }
-  
-                  
-                    
-                    const savePathPoster = path.join(dirPoster, `${postDetails.slug}.jpg`);
-                    const savePathImage = path.join(dirImage, `${postDetails.slug}.jpg`);
+
+
+
+                    const savePathPoster=path.join(dirPoster, `${postDetails.slug}.jpg`);
+                    const savePathImage=path.join(dirImage, `${postDetails.slug}.jpg`);
 
                     const existingPost=await Posts.findOne({ title: postDetails.title });
                     if (existingPost) {
@@ -158,25 +155,25 @@ const main=async () => {
                         // console.log(updatedPost.categories, "\n");
 
                     } else {
-                        
-                         // save the post
-                         downloadImage(postDetails.poster, savePathPoster)
-                         .then(async () => {
-                             
-                             downloadImage(postDetails.image, savePathImage)
-                             .then(async () => {
-                                 console.log('Image downloaded and saved with the correct extension');
-                                 await Posts.create(postDetails);  // Save the new post to the database
-                                 console.log(`Post saved: ${post.title} \n`);
-                             })
-                             .catch((error) => {
-                                 console.error('Failed to download the image:', error);
-                             });
- 
-                         })
-                         .catch((error) => {
-                             console.error('Failed to download the poster:', error);
-                         });
+
+                        // save the post
+                        downloadImage(postDetails.poster, savePathPoster)
+                            .then(async () => {
+
+                                downloadImage(postDetails.image, savePathImage)
+                                    .then(async () => {
+                                        console.log('Image downloaded and saved with the correct extension');
+                                        await Posts.create(postDetails);  // Save the new post to the database
+                                        console.log(`Post saved: ${post.title} \n`);
+                                    })
+                                    .catch((error) => {
+                                        console.error('Failed to download the image:', error);
+                                    });
+
+                            })
+                            .catch((error) => {
+                                console.error('Failed to download the poster:', error);
+                            });
 
                     }
 
@@ -197,10 +194,9 @@ const main=async () => {
                     await page.goto(SITE);  // Default URL if not found
                     await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 60000 }); // Increase to 60 seconds
                 }
-                await scrapePage();
-                
-                const nextButton=await page.$('.nextpostslink');
-                
+
+                const nextButton=await page.$('.next.page-numbers');
+
                 if (nextButton) {
                     console.log("Going to next page...");
                     await Promise.all([
@@ -215,14 +211,16 @@ const main=async () => {
                 } else {
                     console.log("No next page found.");
                 }
-                
+
+
             } catch (error) {
                 console.error("Error going to the next page:", error);
             }
-            
-            // Recursive call to continue scraping the next pageee
 
-            
+            // Recursive call to continue scraping the next pageee
+            await scrapePage();
+
+
         };
         // Start scraping the page
         await scrapePage();
@@ -235,12 +233,12 @@ const main=async () => {
     }
 };
 
-    // Start the server
-    mongoose.connect(process.env.LOCAL_URI).then(() => {
-        console.log("Connected to the database.");
-        console.log("Starting the scraping process... \n");
-        main();
-    }).catch((err) => {
-        console.log("Error connecting to the database...");
-        console.log(err);
-    });  
+// Start the server
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to the database.");
+    console.log("Starting the scraping process... \n");
+    main();
+}).catch((err) => {
+    console.log("Error connecting to the database...");
+    console.log(err);
+});  
